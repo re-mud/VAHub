e2d487036b3309fb96305eff8ef06bed4630633f
reMud <161937535+re-mud@users.noreply.github.com>
2025-07-05 22:48:49 +0500

    feat: add App

---
+++ VAHub/App.cs
@@ -0,0 +1,36 @@
+﻿using VAHub.Logging;
+using VAHub.Services;
+
+namespace VAHub;
+
+public class App
+{
+    private Core _core;
+    private ManualResetEventSlim _mre;
+
+    public App(Core core)
+    {
+        _core = core;
+        _mre = new ManualResetEventSlim(false);
+
+        _core.RecognitionCompleted += Core_RecognitionCompleted;
+    }
+
+    public void Run()
+    {
+        _core.Start();
+        Logger.Info("Приложение запущено");
+        _mre.Wait();
+    }
+
+    public void Exit()
+    {
+        _mre.Set();
+    }
+
+    private void Core_RecognitionCompleted(string text)
+    {
+        Console.WriteLine(text);
+        _core.Speak(text);
+    }
+}
--- VAHub/Program.cs
+++ VAHub/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VAHub;
+using VAHub.Factories;
+using VAHub.Interfaces;
+using VAHub.Logging;
+using VAHub.Managers;
+using VAHub.Options;
+using VAHub.Services;
+
+OptionsManager optionsManager = new OptionsManager("appsettings.json", true);
+CoreFactory coreFactory = new CoreFactory(optionsManager);
+FileLoggerOptions fileLoggerOptions = optionsManager.Get<FileLoggerOptions>("FileLogger");
+CoreOptions coreOptions = optionsManager.Get<CoreOptions>(nameof(Core));
+ILogger logger = new FileLogger(fileLoggerOptions);
+Core core = coreFactory.CreateCore(coreOptions);
+App app = new(core);
+
+Logger.SetLogger(logger);
+
+app.Run();
