bc45b6d671e373be371920b3923ab61bdcb13166
reMud <161937535+re-mud@users.noreply.github.com>
2025-11-01 18:11:43 +0500

    feat(AppCommand): add method from_value

--- vahub/contracts/enums.py
+++ vahub/contracts/enums.py
@@ -1,6 +1,14 @@
 from enum import StrEnum
+from typing import Any
 
 
 class AppCommand(StrEnum):
 	NONE = "none"
 	CLOSE = "close"
+
+	@classmethod
+	def from_value(cls, v: Any):
+		if v is None:
+			return cls.NONE
+		if isinstance(v, str):
+			return cls(v)
