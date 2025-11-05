db73e6ec18f449c92a7bdb242711d4ec081c54ee
reMud <161937535+re-mud@users.noreply.github.com>
2025-11-06 00:00:58 +0500

    feat(contracts): make callable protocols into alias

--- vahub/contracts/protocols.py
+++ vahub/contracts/protocols.py
@@ -1,8 +1,8 @@
 from typing import (
+	Callable,
 	Protocol,
 	TypeAlias,
 	TypeVar,
-	runtime_checkable,
 )
 from .models import SearchResult
 from .enums import AppCommand
@@ -11,31 +11,18 @@ from .enums import AppCommand
 T = TypeVar('T')
 
 
-class Searcher(Protocol[T]):
-	def __call__(self, text: str) -> SearchResult[T]: ...
-
-class OptionsProvider(Protocol):
-	def __call__(self, name: str) -> dict: ...
-
-@runtime_checkable
-class Handler(Protocol):
-	def __call__(self, ctx: "Context", remaining_text: str) -> AppCommand: ...
-
-@runtime_checkable
-class Speaker(Protocol):
-	def __call__(self, text: str) -> None: ...
-
-@runtime_checkable
-class Normalizer(Protocol):
-	def __call__(self, text: str) -> str: ...
-
 class Context(Protocol):
 	def say(self, text: str) -> None: ...
-	def set_context_handler(self, context: Handler) -> None: ...
+	def set_context_handler(self, context: "Handler") -> None: ...
 	def get_context(self) -> "Payload": ...
 	def clear_context(self) -> "Payload": ...
 	def normalize_numbers(self, text: str) -> str: ...
 	def get_options(self, name: str) -> dict: ...
 
 
-Payload: TypeAlias = Handler | None
+OptionsProvider: TypeAlias = Callable[[str], dict]
+Normalizer: TypeAlias = Callable[[str], str]
+Searcher: TypeAlias = Callable[[str], SearchResult[T]]
+Handler: TypeAlias = Callable[[Context, str], AppCommand]
+Speaker: TypeAlias = Callable[[str], None]
+Payload: TypeAlias = "Handler | None"
