0126b0817d8530bd52c01f74d31fd47d43f26f56
reMud <161937535+re-mud@users.noreply.github.com>
2025-11-06 13:46:06 +0500

    feat(vamanifest_manager): add methods

--- vahub/plugins/vamanifest_manager.py
+++ vahub/plugins/vamanifest_manager.py
@@ -1,4 +1,5 @@
 from typing import Any, Callable, Type, TypeVar
+from vahub.contracts import Normalizer, Speaker
 from copy import deepcopy
 
 
@@ -16,17 +17,27 @@ class VAManifestManager:
 			if value is not None and isinstance(value, type_field):
 				fields[key] = deepcopy(value)
 		return fields
+	
+	def get_fields_dict(self, 
+			field: Any, 
+			type_key: Type[T] = object, 
+			type_value: Type[T] = object) -> dict[str, Callable]:
+		fields = self.get_fields(field, dict)
+		filtered_fields = {}
+		for i in fields.values():
+			for k, v in i.items():
+				if isinstance(k, type_key) and isinstance(v, type_value):
+					filtered_fields[k] = v
+		return filtered_fields
+	
+	def get_numbers_normalizers(self) -> dict[str, Normalizer]:
+		return self.get_fields_dict("numbers_normalizers", str, Callable)
+	
+	def get_speakers(self) -> dict[str, Speaker]:
+		return self.get_fields_dict("speakers", str, Callable)
 
 	def get_default_options(self) -> dict[str, dict]:
 		return self.get_fields("default_options", dict)
 	
 	def get_commands(self) -> dict[str, Callable]:
-		fields = self.get_fields("commands", dict)
-		commands: dict[str, Callable] = {}
-		for c in fields.values():
-			if not isinstance(c, dict):
-				continue
-			for k, v in c.items():
-				if isinstance(k, str) and isinstance(v, Callable):
-					commands[k] = v
-		return commands
+		return self.get_fields_dict("commands", str, Callable)
