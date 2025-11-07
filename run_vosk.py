6abaf49b5525d7e6273736b4b4ee77df9ddf4e24
reMud <161937535+re-mud@users.noreply.github.com>
2025-11-07 14:05:47 +0500

    feat: add run_vosk

---
+++ run_vosk.py
@@ -0,0 +1,40 @@
+from vosk import Model, KaldiRecognizer
+import sounddevice as sd
+from queue import Queue
+import bootstrap
+import logging
+import json
+
+
+bootstrap.setup_logging()
+bootstrap.load_config()
+logger = logging.getLogger(__name__)
+vahub = bootstrap.create_vahub()
+
+model_path = bootstrap.config["model"]
+samplerate = bootstrap.config["samplerate"]
+
+queue = Queue()
+model = Model(model_path)
+recognizer = KaldiRecognizer(model, samplerate)
+
+
+def callback(indata, frames, time, status):
+	queue.put(bytes(indata))
+
+try:
+	with sd.RawInputStream(
+			samplerate=samplerate, 
+			blocksize=samplerate // 2, 
+			dtype='int16',
+			channels=1, 
+			callback=callback):
+		while True:
+			data = queue.get()
+			if recognizer.AcceptWaveform(data):
+				text = json.loads(recognizer.Result())["text"]
+				vahub.handle(text)
+except KeyboardInterrupt:
+	pass
+except Exception as e:
+	logger.exception(e)
