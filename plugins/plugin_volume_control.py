50930fdb9f332a7b9e5c35e652a2e33912bad650
reMud <161937535+re-mud@users.noreply.github.com>
2026-05-18 00:22:19 +0500

    feat(plugins): update plugin_volume_control

--- plugins/plugin_volume_control.py
+++ plugins/plugin_volume_control.py
@@ -2,13 +2,10 @@ from pycaw.pycaw import AudioUtilities
 from vahub.contracts import Context
 
 
-device = AudioUtilities.GetSpeakers()
-
-
 def get_manifest() -> dict:
 	return {
 		"name": "Volume control",
-		"version": "1.0",
+		"version": "1.1",
 
 		"default_options": {
 			"volume_step": 10,
@@ -18,53 +15,57 @@ def get_manifest() -> dict:
 
 		"commands": {
 			"громкость|звук": set_volume,
-			"выключи звук|выключить звук|убери звук" :  mute,
-			"включи звук|включить звук|верни звук" :  unmute,
-			"уменьши громкость|уменьшить громкость|понизить громкость|понизь громкость|тише" : volume_down,
-			"увеличь громкость|увеличить громкость|повысить громкость|повысь громкость|громче" : volume_up,
+			"выключи звук|убери звук" :  mute,
+			"включи звук|верни звук" :  unmute,
+			"уменьши громкость|понизь громкость|тише" : volume_down,
+			"увеличь громкость|повысь громкость|громче" : volume_up,
 			"полная громкость|звук на максимум|максимальная громкость" : volume_max,
 			"звук на минимум|минимальная громкость|минимум звука" : volume_min,
 		}
 	}
 
+def _get_ev():
+	return AudioUtilities.GetSpeakers().EndpointVolume
+
+def _set_volume(volume: int) -> None:
+	return _get_ev().SetMasterVolumeLevelScalar(volume, None)
+
+def _get_volume() -> None:
+	return _get_ev().GetMasterVolumeLevelScalar()
+
 def volume_min(context: Context, text: str) -> None:
-	device.EndpointVolume.SetMasterVolumeLevelScalar(
-		context.get_options(__name__)["min_volume"] / 100, 
-		None)
+	_set_volume(context.get_options(__name__)["min_volume"] / 100)
 
 def volume_max(context: Context, text: str) -> None:
-	device.EndpointVolume.SetMasterVolumeLevelScalar(
-		context.get_options(__name__)["max_volume"] / 100, 
-		None)
+	_set_volume(context.get_options(__name__)["max_volume"] / 100)
 
 def mute(context: Context, text: str) -> None:
-	device.EndpointVolume.SetMute(1, None)
+	_get_ev().SetMute(1, None)
 
 def unmute(context: Context, text: str) -> None:
-	device.EndpointVolume.SetMute(0, None)
+	_get_ev().SetMute(0, None)
 
 def set_volume(context: Context, text: str) -> None:
 	volume = get_volume_from_text(context, text)
 	if volume is None:
 		context.say("неверный параметр громкости")
-
-	device.EndpointVolume.SetMasterVolumeLevelScalar(volume, None)
+	_set_volume(volume)
 
 def volume_up(context: Context, text: str) -> None:
 	volume = get_volume_from_text(context, text)
 	if volume is None:
 		volume = context.get_options(__name__)["volume_step"] / 100
 	
-	volume = min(100, device.EndpointVolume.GetMasterVolumeLevelScalar() + volume)
-	device.EndpointVolume.SetMasterVolumeLevelScalar(volume, None)
+	volume = min(100, _get_volume() + volume)
+	_set_volume(volume)
 
 def volume_down(context: Context, text: str) -> None:
 	volume = get_volume_from_text(context, text)
 	if volume is None:
 		volume = context.get_options(__name__)["volume_step"] / 100
 	
-	volume = max(0, device.EndpointVolume.GetMasterVolumeLevelScalar() - volume)
-	device.EndpointVolume.SetMasterVolumeLevelScalar(volume, None)
+	volume = max(0, _get_volume() - volume)
+	_set_volume(volume)
 
 def get_volume_from_text(context: Context, text: str) -> int | None:
 	volume = context.normalize_numbers(text)
